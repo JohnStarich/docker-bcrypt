43c3e2d3b919a406895cb5fe34ca031f087f18b4
John Starich <john.starich@thirdship.com>
2015-09-26 05:19:17 -0500

    Add verify password option.
    Also change to library with the verify capability.

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,4 @@
-import bcrypt
+from passlib.hash import bcrypt
 import sys
 import argparse
 import getpass
@@ -19,20 +19,29 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--password', dest='password', help='The password to hash')
 parser.add_argument('-P', '--password-prompt', dest='password', action=PasswordPrompt, type=str, nargs=0, help='Prompt for the password to hash')
 parser.add_argument('-s', '--strength', type=int, help='Number of hashing rounds to create password hash.')
+parser.add_argument('-v', '--verify', type=str, help='Verify with the provided hash.')
 args = parser.parse_args()
 
 if not args.password:
     print('Error: Please provide a password.', file=sys.stderr)
     parser.print_usage()
     sys.exit(2)
-if args.strength is not None:
-    if args.strength < 4:
-        print('Error: Strength cannot be less than 4.', file=sys.stderr)
-        sys.exit(2)
-    salt = bcrypt.gensalt(rounds=args.strength)
-else:
-    salt = bcrypt.gensalt()
 password = args.password.encode('utf-8')
-bhash = bcrypt.hashpw(password, salt)
-print(bhash.decode('utf-8'), end='', flush=True)
-print(file=sys.stderr)
+if args.verify:
+    verify = args.verify.encode('utf-8')
+    if bcrypt.verify(password, verify):
+        print('Success!', file=sys.stderr)
+        sys.exit(0)
+    else:
+        print('Passwords did not match.', file=sys.stderr)
+        sys.exit(1)
+else:
+    if args.strength is not None:
+        if args.strength < 4:
+            print('Error: Strength cannot be less than 4.', file=sys.stderr)
+            sys.exit(2)
+        bhash = bcrypt.encrypt(password, rounds=args.strength)
+    else:
+        bhash = bcrypt.encrypt(password)
+    print(bhash, end='', flush=True)
+    print(file=sys.stderr)
